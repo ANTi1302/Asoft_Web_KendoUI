9fa93b527bb4165eeba181bfa4a685cab27a3be6
ANTi1302 <porkoe3443334@gmail.com>
2024-05-10 11:03:29 +0700

    update disable maNV

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d49337848e4e0871e3afc93dfe9d7ebc05332be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882575eb4d4a3c47bd788a33f5319298dafdec40")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
